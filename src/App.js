2bd554b27348ca4cff5002762f6c1aad5b1a849a
athulssiorbit <athulss@iorbit-tech.com>
2024-04-25 14:42:00 +0530

    Styled Component package & Folder cleanup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import styled from "styled-components";
 
 
 function App() {
   return (
-   <div>
-    <h1 className="text-3xl md:text-5xl font-bold text-center text-green-500">Hello HAMON</h1>
-   </div>
+   <MainContainer>
+    <div className="container mx-auto px-3 bg-yellow-300">
+      <h1 className="text-3xl md:text-5xl font-bold text-center text-green-500">Hello HAMON</h1>
+    </div>
+   </MainContainer>
   );
 }
 
 export default App;
+
+const MainContainer = styled.div`
+  height: 100vh;
+  width: 100vw;
+  background-color: black;
+`
